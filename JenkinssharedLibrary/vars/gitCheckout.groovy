02807d962bb0c79f8723ae91a2da92975dec4227
Adrija Basu <adrija.tina93@gmail.com>
2024-01-25 22:26:42 +0530

    Update gitCheckout.groovy

--- JenkinssharedLibrary/vars/gitCheckout.groovy
+++ JenkinssharedLibrary/vars/gitCheckout.groovy
@@ -3,6 +3,6 @@ def call(Map stageParams)
     checkout{[
         $class: 'GitSCM'
         branches: [[ name: stageparams.branch ]]
-        userRemoteConfigs: [[ url: stageparams.url]]
+        userRemoteConfigs: [[ url: stageparams.url ]]
     ]}
 }
